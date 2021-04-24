9ecc7a5815de1c9c3a10b6ee7e3dea5280cf3584
Len Smith <len@barrison.com>
2021-04-23 18:05:40 -0700

    Add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Settings from './components/Settings';
 import Timer from './components/Timer';
 
 function App() {
   return (
     <div className="App">
-      <Timer />
+      <Router>
+        <Switch>
+          <Route path="/settings">
+            <Settings />
+            <Link to="/">back</Link>
+          </Route>
+          <Route path="/">
+            <Timer />
+          </Route>
+        </Switch>
+        <Link to="/settings">settings</Link>
+      </Router>
     </div>
   );
 }
